befbfabb3f607e91f2cbc794093c36d5886747cf
Will Harris <william.harris108@gmail.com>
2021-02-12 15:34:53 -0600

    Added in guards, some roads, and did some scritping
    I made each guard variety a prefab so that we can spawn them from an array. If there is a more efficient way, feel free to change it. I also made it so they spawn close to a stamp. Scripting their patrol and animation should be next.

--- StampCrimeSnailSpear/Assets/Scripts/SpawnStamps.cs
+++ StampCrimeSnailSpear/Assets/Scripts/SpawnStamps.cs
@@ -8,6 +8,7 @@ public class SpawnStamps : MonoBehaviour
     public float xRandom = 15f;
     public float yRandom = 15f;
 
+    public GameObject[] guards;
     public GameObject stamp;
     public int maxStampCount;
 
@@ -31,6 +32,9 @@ public class SpawnStamps : MonoBehaviour
     {
         GameObject stampInstance = Instantiate(stamp);
         stampInstance.transform.position = new Vector2(Random.Range(-xRandom, xRandom), Random.Range(-yRandom, yRandom));
+        Vector2 stampPos = stamp.transform.position;
+        GameObject guardInstance = Instantiate(guards[Random.Range(0, 2)]);
+        guardInstance.transform.position = new Vector2(stampPos.x + (Random.Range(-3, 3)), stampPos.y + (Random.Range(-3, 3)));
         GameManager.Instance.IncrementStampCount(1);
     }
 }
